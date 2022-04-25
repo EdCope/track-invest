29f5938da765cf02de0874fbd70c120a45ddb87c
Ed <ed321@hotmail.co.uk>
2022-04-25 18:50:55 +0100

    Setup Backend testing on the routes - "/" returns HTML

---
+++ server/routes/index.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app.js');
+
+describe('GET /', () => {
+  it('responds with html', async () => {
+    const response = await request(app).get('/')
+    expect(response.status).toBe(200);
+    expect(response.header['content-type']).toBe('text/html; charset=utf-8')
+  })
+})
