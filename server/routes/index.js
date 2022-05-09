6c0015308b751efe66e5e72c51358f2ff29ffb8f
Ed <ed321@hotmail.co.uk>
2022-05-09 16:09:24 +0100

    Display multiple trackers on home route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async (req, res) => {
-  const priceReq = await axios.get('http://localhost:3000/asset/aapl');
-  res.render('index', { title: 'Track Invest', price: priceReq.data.price });
+  const trackers = await axios.get('http://localhost:3000/asset/trackers', {
+  data: {
+    "user": "62710e9962c1c9b2cf62df2e",
+  }})
+  const track = await Promise.all(trackers.data.map(async (tracker) => {
+    const trackerPrice = await axios.get(`http://localhost:3000/asset/${tracker.tickername}`)
+    return {...tracker, price: trackerPrice.data.price};
+  }));
+  //const priceReq = await axios.get('http://localhost:3000/asset/aapl');
+  
+  res.render('index', { title: 'Track Invest', trackers: track });
 });
 
 module.exports = router;
